2f564d9287f472bd8d7f0d225eaa7776e1596957
Hung Vo <vhmh2005@users.noreply.github.com>
2016-09-04 11:57:45 +0700

    Update versions

--- packages/gridfs/package.js
+++ packages/gridfs/package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'cfs:gridfs',
-  version: '0.0.27',
+  version: '0.0.34',
   summary: 'GridFS storage adapter for CollectionFS',
   git: 'https://github.com/CollectionFS/Meteor-cfs-gridfs.git'
 });
 
 Npm.depends({
-  mongodb: '1.3.23',
+  mongodb: '2.2.9',
   'gridfs-stream': '0.5.3'
   //'gridfs-locking-stream': '0.0.3'
 });
@@ -14,7 +14,7 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
-  api.use(['cfs:base-package@0.0.27', 'cfs:storage-adapter@0.1.1']);
+  api.use(['cfs:base-package@0.0.30', 'cfs:storage-adapter@0.2.3']);
   api.addFiles('gridfs.server.js', 'server');
   api.addFiles('gridfs.client.js', 'client');
 });
